1a060bbe1508dd9d229284cdd864da1cdf7f8dbe
Sebastian Guerra <spguerraespinoza@gmail.com>
2021-11-22 20:13:24 -0300

    cree base de modelo 'Test'

--- proyecto_iwg101/page/models.py
+++ proyecto_iwg101/page/models.py
@@ -7,7 +7,12 @@ class Area(models.Model):
     pass
 
 class Test(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+    
 
 class Pregunta(models.Model):
     id = models.AutoField(primary_key=True)
