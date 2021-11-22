4c0fe7ef08081ded90528728a012780bca4e201e
Sebastian Guerra <spguerraespinoza@gmail.com>
2021-11-22 19:19:47 -0300

    Cree clases principales para la bd

--- proyecto_iwg101/page/models.py
+++ proyecto_iwg101/page/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Area(models.Model):
+    pass
+
+class Test(models.Model):
+    pass
+
+class Pregunta(models.Model):
+    pass
+
+class Test_Realizacion(models.Model):
+    pass
+
+class Respuesta(models.Model):
+    pass
