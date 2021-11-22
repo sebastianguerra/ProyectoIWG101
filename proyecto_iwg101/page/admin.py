5ccef11a3301a90a7ddc2207df59ad418783b6cc
Sebastian Guerra <spguerraespinoza@gmail.com>
2021-11-22 20:18:15 -0300

    registre los modelos en el archivo 'admin.py'

--- proyecto_iwg101/page/admin.py
+++ proyecto_iwg101/page/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from page.models import Area, Test, Pregunta, Test_Realizacion, Respuesta
 
 # Register your models here.
+
+admin.site.register(Area)
+admin.site.register(Test)
+admin.site.register(Pregunta)
+admin.site.register(Test_Realizacion)
+admin.site.register(Respuesta)
