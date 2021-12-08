1d800650ed637df58776602f620bbd77845e0006
Sebastian Guerra <spguerraespinoza@gmail.com>
2021-12-07 21:54:07 -0300

    Engine djongo

--- proyecto_iwg101/settings.py
+++ proyecto_iwg101/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'proyecto_iwg101.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'iwg101',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': os.environ.get("DATABASE_URI")
+        }  
     }
 }
 
