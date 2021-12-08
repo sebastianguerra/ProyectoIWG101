b59a0ab5e0372c1040ccca2aa9307306ce6aa46f
Sebastian Guerra <spguerraespinoza@gmail.com>
2021-12-08 00:45:27 -0300

    Modifique el STATIC_ROOT

--- proyecto_iwg101/settings.py
+++ proyecto_iwg101/settings.py
@@ -149,4 +149,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = 'staticfiles'
